dae5016c643c4001319ade7935ff53937dea7188
Saptarshi Bhattacharya <saptarshib98@gmail.com>
2023-02-04 16:25:41 +0530

    add support for deployment

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.post("/", (req, res) => {
     request.end()
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`server is listening to port 3000`)
 })
